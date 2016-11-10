7add8dfdd3ce0acec8d4aa80f7d58329ab2d54fb
climaco02 <gabrielsclimaco@gmail.com>
2016-11-10 09:18:44 -0200

    getting profiles from database and print them

--- perfis/views.py
+++ perfis/views.py
@@ -5,14 +5,7 @@ def index(request):
 	return render(request, 'index.html')
 
 def exibir(request, perfil_id):
-    print 'ID do perfil recebido: %s' % (perfil_id)
-
-    perfil = Perfil()
-
-    if perfil_id == '1':
-    	perfil = Perfil('Gabriel Climaco', 'gabrielsclimaco@gmail.com', '99293-2523', 'fga')
-
-    if perfil_id == '2':
-    	perfil = Perfil('Isabela Climaco', 'isabelasclimaco@gmail.com', '98172-6597', 'fga')
-
+    # if perfil_id == '2':
+    # 	perfil = Perfil('Isabela Climaco', 'isabelasclimaco@gmail.com', '98172-6597', 'fga')
+    perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfil.html', {"perfil" : perfil})
